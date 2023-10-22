ea46740868cd7bbc5bbc8469348cae5c63f7e2c8
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-22 13:30:11 +0600

    16:02

--- app/src/main/java/com/aikei/notes/MainActivity.kt
+++ app/src/main/java/com/aikei/notes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aikei.notes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aikei.notes.databinding.ActivityMainBinding
@@ -8,10 +9,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
