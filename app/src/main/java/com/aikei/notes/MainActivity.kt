84862f2744447d1280508c74672420a1da569c22
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-22 13:10:04 +0600

    Adding all files

---
+++ app/src/main/java/com/aikei/notes/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.aikei.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aikei.notes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
